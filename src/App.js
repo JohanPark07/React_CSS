5c11be1145cc820ceb826bba79f90c95578c2c91
Johan <sejongbgy07@gmail.com>
2022-10-26 23:21:56 +0900

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import TailwindComponent from "./TailwindComponent";
 
 function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <TailwindComponent />
     </>
   );
 }
